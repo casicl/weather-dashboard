5ceec503d7fb3d38bd48b6377c37e361e10f6949
Casi Clarkson <casiclarkson@gmail.com>
2024-05-05 13:24:12 -0700

    added dayjs to 5 day array

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ var searchBtn = document.querySelector("#city-submit");
 var mainWeather = document.querySelector(".main-weather");
 var fiveDayEl = document.querySelector(".five-day");
 
+//day.js plugins
+// dayjs.extend(window.dayjs_plugin_utc);
+// dayjs.extend(window.dayjs_plugin_timezone);
+
 //city search function
 function citySearchSubmit(event) {
   event.preventDefault();
@@ -14,32 +18,40 @@ function citySearchSubmit(event) {
   searchWeather(city);
   console.log(event);
 }
+//how to local storage, i have no idea
+function appendToHistory(search) {
+  if (cityHistory.indexOf(search) !== -1) {
+    return;
+  }
+  cityHistory.push(search);
+
+  localStorage.setItem("city-history", JSON.stringify(cityHistory));
+  renderCityHistory();
+}
 //const cityHistory = JSON.parse(localStorage.getItem("cityHistoryEl")) ||[];
 //const cityHistoryEl = document.getElementById("city-history");
 //cityHistoryEl(item => {
-  //  const cityHistory = document.createElement("div");
-    //cityHistory.textContent = item.name;
-    //displayElement.appendChild(itemElement);
+//  const cityHistory = document.createElement("div");
+//cityHistory.textContent = item.name;
+//displayElement.appendChild(itemElement);
 //});
 
 //fetch api
 function searchWeather(city) {
-
-    
-
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
   )
     .then(function (response) {
       return response.json();
-     
     })
     .then(function (data) {
       var weatherIcon = data.weather[0].icon;
       var weatherImageURL = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
       var todaysDate = new Date(data.dt * 1000).toLocaleString().split(",")[0];
+      console.log(todaysDate)
 
       //added html elements with weather data
+      //var date = dayjs().format('M/D/YYYY');
       var currentWeather = `
         <h2>${data.name}</h2>
         <span>${todaysDate}</span>
@@ -50,8 +62,7 @@ function searchWeather(city) {
         <p>windspeed: ${data.wind.speed}mph</p>
         
         `;
-      
-       
+
       //getting lat and lon data
       mainWeather.innerHTML = currentWeather;
       var lon = data.coord.lon;
@@ -71,7 +82,7 @@ function searchWeather(city) {
           for (var i = 0; i < fiveDayArray.length; i++) {
             fiveDayCard += `
                     <div>
-
+                    <h4>${dayjs.unix(fiveDayArray[i].dt).format("MM/DD/YYYY")}</h4>
                     <h4>${fiveDayArray[i].main.temp}°F</h4>
                     <h4>${fiveDayArray[i].main.humidity}%</h4>
                     <h4>${fiveDayArray[i].wind.speed}mph</h4>
@@ -85,8 +96,35 @@ function searchWeather(city) {
         });
     });
 }
-//add cities to local storage and display history
 
+//city search function
+function citySearchSubmit(event) {
+  event.preventDefault();
+  console.log("click");
+  var city = searchForm.value.trim();
+  searchWeather(city);
+  for (let i = 0; i < cityHistory.length; i++) {
+    const btn = document.createElement("button");
+    btn.textContent = cityHistory[i];
+    searchHistoryContainer.append(btn);
+    btn.setAttribute("city-search", cityHistory[i]);
+  }
+
+  console.log(event);
+}
+//how to local storage, i have no idea
+//add cities to local storage and display history
+var cityHistory = [];
+var displayCityHistory = document.getElementById("city-history");
+
+function appendToHistory(search) {
+  if (cityHistory.indexOf(search) !== -1) {
+    return;
+  }
+  cityHistory.push(search);
+
+  localStorage.setItem("city-history", JSON.stringify(cityHistory));
+  renderCityHistory();
+}
 //event listener to submit form for city search
 searchBtn.addEventListener("click", citySearchSubmit);
-
